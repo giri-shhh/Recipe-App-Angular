ca947595cc13d966ed95c9ca18b68e55ec2e94de
Girish <pgirisha77@gmail.com>
2019-11-10 18:13:46 +0530

    Recipe Edit component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -9,7 +10,9 @@ const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipesComponent, children: [
         {path: '', component: RecipeStartComponent},
-        {path: ':id', component: RecipeDetailComponent}
+        {path: 'new', component: RecipeEditComponent},
+        {path: ':id', component: RecipeDetailComponent},
+        {path: ':id/edit', component: RecipeEditComponent}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
