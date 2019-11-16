524c03c91cbfd5d305e49d144019860537f1fd5f
Girish <pgirisha77@gmail.com>
2019-11-16 20:24:14 +0530

    recipe edit and add component updated

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,22 +8,6 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
-   /*  private recipes: Recipe[] = [
-        new Recipe('First Recipe',
-            'This is simply a test', 'https://www.hap.org/-/media/blog/images/post-images/201812/hap-minestrone-600x300.jpg?h=300&w=600&la=en&hash=56BCA000C02ABB32467E4F36C148C4B4A095CC30',
-            [
-                new Ingredient('Meat', 12),
-                new Ingredient('French Fries', 25)
-            ]
-        ),
-        new Recipe('Second Recipe',
-            'This is simply a test', 'https://tasteasianfood.com/wp-content/uploads/2015/10/beef-rendang-3a-600x300.jpg',
-            [
-                new Ingredient('Buns', 20),
-                new Ingredient('Meat', 20)
-            ])
-    ]; */
-
     private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) { }
