9bceadc3f69e028ab4674472fa6732efc5499183
Girish <pgirisha77@gmail.com>
2019-11-16 22:24:01 +0530

    firebase login functionality added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-auth',
@@ -22,20 +23,26 @@ export class AuthComponent {
         }
         const email = form.value.email;
         const password = form.value.password;
+
+        let authObs: Observable<AuthResponseData>;
+
         this.isLoading = true;
+
+
         if (this.isLoginMode) {
+            authObs = this.authService.login(email, password);
 
         } else {
-            this.authService.signUp(email, password)
-                .subscribe(resData => {
-                    console.log(resData);
-                    this.isLoading = false;
-                },
-                    errorMessage => {
-                        this.error = errorMessage;
-                        this.isLoading = false;
-                    });
+            authObs = this.authService.signUp(email, password);
         }
+        authObs.subscribe(resData => {
+            console.log(resData);
+            this.isLoading = false;
+        },
+            errorMessage => {
+                this.error = errorMessage;
+                this.isLoading = false;
+            });
         form.reset();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    register?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -23,15 +24,28 @@ export class AuthService {
                 email,
                 password,
                 returnSecureToken: true
-            }).pipe(catchError(errorRes => {
-                let errorMessage = 'An unkown error occurred!';
-                if (!errorRes.error || !errorRes.error.error) {
-                    return throwError(errorMessage);
-                }
-                switch (errorRes.error.error.message) {
-                    case 'EMAIL_EXISTS': errorMessage = 'This email exists already!';
-                }
-                return throwError(errorMessage);
-            }));
+            }).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+        // tslint:disable-next-line: max-line-length
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAf6Ie8l7UbTLnB1k3jfdAqJmw9UCqza18', {
+            email,
+            password,
+            refreshToken: true
+        }).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unkown error occurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS': errorMessage = 'This email exists already!'; break;
+            case 'EMIAL_NOT_FOUND': errorMessage = 'This Email does not exist'; break;
+            case 'INVALID_PASSWORD': errorMessage = 'Password is incorrect'; break;
+        }
+        return throwError(errorMessage);
     }
 }
